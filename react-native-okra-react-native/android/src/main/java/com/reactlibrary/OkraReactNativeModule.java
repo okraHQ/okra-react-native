3c9d1be3fa4f8e10181ed1c9d549868556286670
eshiett1995 <eshiett1995@gmail.com>
2019-12-05 11:32:45 +0100

    wip - added method openOkraWidget to Android and IOS
    wip - added method openOkraWidget to Android and IOS

--- react-native-okra-react-native/android/src/main/java/com/reactlibrary/OkraReactNativeModule.java
+++ react-native-okra-react-native/android/src/main/java/com/reactlibrary/OkraReactNativeModule.java
@@ -24,4 +24,15 @@ public class OkraReactNativeModule extends ReactContextBaseJavaModule {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
+
+    @ReactMethod
+    public void openOkraWidget(ReadableMap options){
+        ArrayList  products = new ArrayList<Enums.Product>();
+        for(int index = 0; index < options.getArray("products").size(); index++){
+            String product = options.getArray("products").getString(index);
+            products.add(Enums.Product.valueOf(product));
+        }
+        OkraOptions okraOptions = new OkraOptions(true, options.getString("key"),options.getString("token"), products, Enums.Environment.valueOf(options.getString("environment")),options.getString("clientName"));
+        Okra.create(getReactApplicationContext(), okraOptions);
+    }
 }
--- react-native-okra-react-native/ios/OkraReactNative.m
+++ react-native-okra-react-native/ios/OkraReactNative.m
@@ -1,14 +1,16 @@
 #import "OkraReactNative.h"
+#import <OkraWidget/OkraWidget-Swift.h>
+
+ @implementation MyFancyLibrary
+ RCT_EXPORT_MODULE()
+ RCT_EXPORT_METHOD(greetings:(RCTResponseSenderBlock)callback){
+     
+     [Okra createWithBaseController:self okraOptions:[[OkraOptions alloc] initWithIsWebview: <#(BOOL)#>  key:@"c81f3e05-7a5c-5727-8d33-1113a3c7a5e4" token:@"5d8a35224d8113507c7521ac" products:[NSArray arrayWithObjects:@"auth",@"transactions", nil] env:@"production" clientName:@"John"]];
+
+   
+   NSString* someString = @"crash";
+   callback(@[someString]);
+ }
+ @end
 
 
-@implementation OkraReactNative
-
-RCT_EXPORT_MODULE()
-
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
-}
-
-@end
